30e5bf0aa2f431d1ca329a0b184bfda2db90ebc8
Santiago <santiago@MacBook-Pro-de-Santiago.local>
2023-01-17 22:50:22 -0500

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+stop.disabled = true;
+let timerId = null;
+
+
+
+start.addEventListener("click", () => {
+    start.disabled = true;
+    stop.disabled = false;
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+        console.log(color);
+    }, 1000)
+  });
+
+stop.addEventListener("click", () => {
+    start.disabled = false;
+    stop.disabled = true;
+    clearInterval(timerId);
+});
 
